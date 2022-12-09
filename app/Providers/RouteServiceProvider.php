88f0e5533f3e230d0abc61f732352d2d3a59b5de
Uendel <uendel_silveira@outlook.com>
2022-12-09 10:46:27 -0300

    Tenant Running

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,17 +24,28 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
+//    public function boot()
+//    {
+//        $this->configureRateLimiting();
+//
+//        $this->routes(function () {
+//            Route::middleware('api')
+//                ->prefix('api')
+//                ->group(base_path('routes/api.php'));
+//
+//            Route::middleware('web')
+//                ->group(base_path('routes/web.php'));
+//        });
+//    }
+
+
     public function boot()
     {
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            $this->mapApiRoutes();
+            $this->mapWebRoutes();
         });
     }
 
@@ -43,10 +54,37 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+    }
+
+    protected function mapWebRoutes()
+    {
+        foreach ($this->centralDomains() as $domain) {
+            Route::middleware('web')
+                ->domain($domain)
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+        }
+    }
+
+    protected function mapApiRoutes()
+    {
+        foreach ($this->centralDomains() as $domain) {
+            Route::prefix('api')
+                ->domain($domain)
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+        }
+    }
+
+    protected function centralDomains(): array
+    {
+        return config('tenancy.central_domains');
     }
 }
